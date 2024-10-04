470616c8a49bd93e2e42da8d1cc577b646650183
rumronya <rum.ronya@gmail.com>
2024-10-04 10:36:54 +0300

    2 new classes

---
+++ src/logicnovellas/NovellaGame.java
@@ -0,0 +1,4 @@
+package logicnovellas;
+
+public class NovellaGame {
+}
---
+++ src/users/User.java
@@ -0,0 +1,15 @@
+package users;
+
+import java.util.Date;
+
+public class User {
+    private int ID;
+    private String login;
+    private String password;
+    private String email;
+    private String img;
+    private Date birthday;
+    private int countCreateGames;
+    private int countPlayGames;
+    //lists??
+}
