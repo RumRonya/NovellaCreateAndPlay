b78b0a07193008de693d599c422a9be0b2e68556
rumronya <rum.ronya@gmail.com>
2024-10-04 10:50:55 +0300

    class NovellaGame - field

--- src/logicnovellas/NovellaGame.java
+++ src/logicnovellas/NovellaGame.java
@@ -1,5 +1,15 @@
 package logicnovellas;
 
+import java.util.Date;
+
 public class NovellaGame {
     private int ID;
+    private String name;
+    private String poster;
+    private String description;
+    private int rating;
+    private Date createDate;
+    private Date updateDate;
+    private int ageRestriction;
+    private String duration;
 }
